545d4f50cbf78ce3548fd5220f5dc02a426cbc41
Saifudheen <saifuhameed@gmail.com>
2025-12-14 18:59:13 +0300

    Update pre-install.sh

--- pre-install.sh
+++ pre-install.sh
@@ -1,19 +1,25 @@
 #!/bin/bash
+RED='\e[0;31m'
+GREEN='\e[0;32m'
+BLUE='\e[0;34m'
+NC='\e[0m' # No Color (resets to default)
+
+echo "${BLUE}Updating apt.${NC}"
 apt update && apt upgrade -y
 
-
+echo "${BLUE}Installing authbind.${NC}"
 apt-get install authbind
 cd /etc/authbind
 touch /etc/authbind/byport/80
 chown debian:debian /etc/authbind/byport/80
+echo "${BLUE}Stopping nginx if running${NC}"
 systemctl stop nginx
 systemctl disable nginx
 
+echo "${BLUE}Installing essential libraries to run modbus app${NC}"
 apt-get -y build-essential libmodbus-dev  libcjson-dev libsqlite3-dev libhiredis-dev python3-venv python3-pip
 
-runuser -l debian -c "git clone https://github.com/saifuhameed/iot-bare-stack.git"
-
-cd modbus 
+cd modbus
 runuser -l debian -c "gcc -o  modbus_to_redis  modbus_to_redis.c config.c -lmodbus -lcjson -lsqlite3 -lhiredis"
 
 #must reboot
